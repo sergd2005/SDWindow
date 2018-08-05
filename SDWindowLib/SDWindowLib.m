285fd1305c179a9ffadb6847ca43811d2abe32f2
dankevyc <dankevyc@adobe.com>
2018-08-04 18:18:07 -0700

    adding static lib

---
+++ SDWindowLib/SDWindowLib.m
@@ -0,0 +1,13 @@
+//
+//  SDWindowLib.m
+//  SDWindowLib
+//
+//  Created by dankevyc on 8/4/18.
+//  Copyright © 2018 sdsoft. All rights reserved.
+//
+
+#import "SDWindowLib.h"
+
+@implementation SDWindowLib
+
+@end
